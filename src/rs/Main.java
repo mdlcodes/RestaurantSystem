a9714569b9de2af5aa401e4f200c3443f4e02bdc
mdlcodes <michelle.digay.landicho@gmail.com>
2026-04-28 11:06:08 +0800

    Added a validity checker for user input.

--- src/rs/Main.java
+++ src/rs/Main.java
@@ -16,7 +16,7 @@ public class Main {
 			System.out.println("4.Manage Available Seats");
 			System.out.println("5.Manage Billing Records");
 			System.out.println("6.Exit");
-			int choice = sc.nextInt();
+			int choice = getPositiveInteger("Enter choice: ", 1, 6);
 			
 			while(true) {
 				switch(choice) 
@@ -49,15 +49,58 @@ public class Main {
 		System.out.println("3. Remove Reservation");
 		System.out.println("4. Search Reservation");
 		System.out.println("5. Back to Main Menu");
-		int choice = sc.nextInt();
+		int choice = getPositiveInteger("Enter choice: ", 1, 5);
 		
 	}
 	
-	public static void foodOrder() {}
+	public static void foodOrder() {
+		System.out.println("1. Add Food Order");
+		System.out.println("2. Serve Food Order");
+		System.out.println("3. View Pending Order");
+		System.out.println("4. Back to main menu");
+
+	}
 	
-	public static void menuCategories() {}
-	public static void availSeats() {}
-	public static void billing() {}
+	public static void menuCategories() {
+		System.out.println("1. Add Category");
+		System.out.println("2. View Category");
+		System.out.println("3. Back to Main Menu");
+	}
+	public static void availSeats() {
+		System.out.println("1. Add Table Numbers");
+		System.out.println("2. Remove Occupied Table");
+		System.out.println("3. View available Tables");
+		System.out.println("4. Back to main menu");
+
+	}
+	public static void billing() {
+		System.out.println("1. Add billing record");
+		System.out.println("2. View Billing Record");
+		System.out.println("3. Search bill by customer");
+		System.out.println("4. Back to main menu");
+	}
 	
+	public static int getPositiveInteger(String prompt, int min, int max) {
+		while(true) {
+			try {
+				System.out.print(prompt);
+				int input = sc.nextInt();
+				sc.nextLine();
+				
+				if(input < min || input > max) {
+					System.err.println("Invalid Choice!");
+					continue;
+				} 
+				
+				return input;
+				
+			}catch(InputMismatchException ime) {
+				System.err.println("Invalid Input");
+				sc.nextLine();
+			}
+		}
+		
+		
+	}
 	
 }
